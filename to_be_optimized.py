764f05f2c5085756d5a793878212ab6224a9eb52
Rodrigo Anderson <rodrigo.anderson@emcasa.com>
2023-08-31 20:17:02 -0300

    feat: clean code

--- to_be_optimized.py
+++ to_be_optimized.py
@@ -1,28 +1,44 @@
-from time import time, sleep
-TIME_DELAY = 3 # Don’t change this variable
+from time import sleep
+from time import time
+
+
+TIME_DELAY = 3  # Don’t change this variable
+
 
 class Client:
+    """Client class"""
+
     def __init__(self) -> None:
         self.time_delay = TIME_DELAY
-    def request(self):
+
+    def request(self) -> None:
+        """Client request"""
         sleep(self.time_delay)
+
+
 class Server:
-    def __init__(self):
+    """Server class"""
+
+    def __init__(self) -> None:
         self.time_delay = TIME_DELAY
-        def run(self, request):
-            self.listen(request)
-            print("starting server...")
-            sleep(self.time_delay)
-            print("The work is done!!!")
-        def listen(self, client_request):
-            print("listening event...")
-            client_request()
-            print("processing event")
 
-if __name__ == '__main__':
+    def run(self, request: None) -> None:
+        """Run method"""
+        self.listen(request)
+        print("Starting server...")  # noqa T201
+        sleep(self.time_delay)
+        print("The work is done!!!")  # noqa T201
 
-	server = Server()
-	client = Client()
-	t0 = time()
-	server.run(client.request)
-	print(f"time spent: {time() - t0}")
+    def listen(self, client_request: None) -> None:
+        """Listen method"""
+        print("Listening event...")  # noqa T201
+        client_request()
+        print("Processing event")  # noqa T201
+
+
+if __name__ == "__main__":
+    server = Server()
+    client = Client()
+    t0 = time()
+    server.run(client.request)
+    print(f"Time spent: {time() - t0}")  # noqa T201
