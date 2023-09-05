d7a97dc86c3a680eac0f902c153c2c5486560fc5
Rodrigo Anderson <rodrigo.anderson@emcasa.com>
2023-09-05 11:17:40 -0300

    feat: Add error handler decorator to stratgies classes

--- data_transformation/strategies/to_lowercase_strategy.py
+++ data_transformation/strategies/to_lowercase_strategy.py
@@ -1,9 +1,11 @@
 from data_transformation.strategies.base_strategy import TransformationStrategy
+from data_transformation.utils.error_handler import ErrorHandler
 
 
 class ToLowercaseStrategy(TransformationStrategy):
     """Strategy to transform the given string to lowercase."""
 
+    @ErrorHandler.handle_errors("Error on ToLowercaseStrategy.transform execution.")
     def transform(self, var1: str, var2: None = None) -> str:
         """
         Transform the given string to lowercase.
