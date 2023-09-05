8bb87cea38d6c6326b1648a792cca4c60f44b583
Rodrigo Anderson <rodrigo.anderson@emcasa.com>
2023-09-05 10:07:11 -0300

    fix: docstrig adjustment

--- data_transformation/strategies/subtract_strategy.py
+++ data_transformation/strategies/subtract_strategy.py
@@ -6,13 +6,13 @@ class SubtractStrategy(TransformationStrategy):
 
     def transform(self, var1: float, var2: float) -> float:
         """
-        Add two numbers together.
+        Subtract two numbers together.
 
         Parameters:
-        - var1 (float): First number to be added.
-        - var2 (float): Second number to be added.
+        - var1 (float): First number.
+        - var2 (float): Second number to be subtracted from the first.
 
         Returns:
-        float: The sum of the two numbers.
+        float: The subtraction of the two numbers.
         """
         return var1 - var2
